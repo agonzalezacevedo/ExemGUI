1ea325c623017fd1da5e184c6aaac0146bd5827d
agonzalezacevedo <56076787+agonzalezacevedo@users.noreply.github.com>
2020-03-29 20:38:00 +0200

    Nuevos ejemplos de uso ventanas

--- src/exemgui1/ExemGUI1.java
+++ src/exemgui1/ExemGUI1.java
@@ -3,8 +3,6 @@ package exemgui1;
 public class ExemGUI1 {
 
     public static void main(String[] args) {
-        Layouts ven = new Layouts();
-        ven.disGridLayout();
+        EventosClaseAnonima ev = new EventosClaseAnonima();
     }
-    
 }
